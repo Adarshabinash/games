3f33fa21eaa221a75851652e3351222588425f1d
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2025-04-15 15:35:14 +0530

    Winner logo printed

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./styles/TicTacToe.css";
 import circleIcon from "../assets/circle.png";
 import crossIcon from "../assets/cross.png";
@@ -8,6 +8,7 @@ let data = ["", "", "", "", "", "", "", "", ""];
 const TicTacToe = () => {
   let [count, setCount] = useState(0);
   const [lock, setLock] = useState(false);
+  let titleRef = useRef(null);
 
   const toggle = (e, num) => {
     if (lock) {
@@ -31,33 +32,42 @@ const TicTacToe = () => {
 
   const checkWin = () => {
     if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
-      won(data);
+      won(data[2]);
     } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
-      won(data);
+      won(data[5]);
     } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
-      won(data);
+      won(data[8]);
     } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
-      won(data);
+      won(data[8]);
     } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
-      won(data);
+      won(data[6]);
     } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
-      won(data);
+      won(data[6]);
     } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
-      won(data);
+      won(data[7]);
     } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
-      won(data);
+      won(data[8]);
     }
   };
 
   const won = (winner) => {
     setLock(true);
+    if (winner === "x") {
+      titleRef.current.innerHTML = `Congratulations:  <img src=${crossIcon} style="height: 30px; width: 30px;" />            wins`;
+    } else {
+      titleRef.current.innerHTML = `Congratulations:  <img src=${circleIcon} style="height: 30px; width: 30px;" />            wins`;
+    }
   };
 
   const resetBoard = () => {};
 
+  console.log("data------------->", data);
+
   return (
     <div className="container">
-      <h1 className="title">Tic Tac Toe</h1>
+      <h1 className="title" ref={titleRef}>
+        Tic Tac Toe
+      </h1>
       <div className="board">
         <div className="row1">
           <div className="boxes" onClick={(e) => toggle(e, 0)}></div>
